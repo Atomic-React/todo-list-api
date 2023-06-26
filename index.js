34b3cd0d0b936bc5b7f0ed9501721e49c5138eb7
Bryan BERGER <contact@weberger.net>
2023-06-26 15:21:29 +0200

    add cors policy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const tasksRouter = require('./routes/tasks.router');
 const db = require('./config/database.config');
 
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
